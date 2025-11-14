1002306fd4df07110bf0428b8155e96f00cb0ce4
Mona-Darwish <monafathydarwish94@gmail.com>
2025-11-14 02:01:36 +0200

    mona edit Pom pattern for LoginPage

--- ALX3_SWD6_G1_NHA-202/src/main/java/POMpatternPages/LoginPage.java
+++ ALX3_SWD6_G1_NHA-202/src/main/java/POMpatternPages/LoginPage.java
@@ -1,4 +1,32 @@
 package POMpatternPages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class LoginPage {
+
+    // variables
+    private WebDriver driver;
+
+    //locators
+     private final By username=By.id("user-name");
+     private final By password= By.id("password");
+     private final By loginButton=By.xpath("//input[@type='submit']");
+
+
+     // constructor
+    public LoginPage(WebDriver driver){
+        this.driver= driver;
+    }
+
+
+    // Actions
+    public void setLogin(String userName, String pass){
+        driver.findElement(username).sendKeys();
+        driver.findElement(password).sendKeys();
+        driver.findElement(loginButton).click();
+
+    }
+
+    // validation
 }
