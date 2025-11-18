8342eae9cb6e12ad9572016ba30be56b15e1b916
Mona-Darwish <monafathydarwish94@gmail.com>
2025-11-18 03:52:55 +0200

    edit bot2

--- ALX3_SWD6_G1_NHA-202/src/main/java/Bot/ActionsBot.java
+++ ALX3_SWD6_G1_NHA-202/src/main/java/Bot/ActionsBot.java
@@ -2,6 +2,8 @@ package Bot;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class ActionsBot {
 
@@ -13,29 +15,71 @@ public class ActionsBot {
 
 
     // constructors
-    public ActionsBot (WebDriver driver){
-        this.driver=driver;
-        this.waitBots=new WaitBots(driver);
+    public ActionsBot(WebDriver driver) {
+        this.driver = driver;
+        this.waitBots = new WaitBots(driver);
 
     }
 
 
     // Actions
-     public void clicking(By locator){
+    public void clicking(By locator) {
 
-     }
+        waitBots.fluentWait().until(d -> {
+                    try {
+                        WebElement element = d.findElement(locator);
+                        new Actions(d).scrollToElement(element);
+                        element.click();
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+        );
+    }
 
 
+    public void typing(By locator, String text) {
 
-    public void typing(By locator, String text){
+        waitBots.fluentWait().until(d -> {
+                    try {
+                        WebElement element = d.findElement(locator);
+                        new Actions(d).scrollToElement(element);
+                        element.clear();
+                        element.sendKeys(text);
+                        return true;
+                    } catch (Exception e) {
+                        return false;
+                    }
+                }
+        );
+
 
     }
 
 
     public String getText(By locator) {
-        return null;
+
+       return waitBots.fluentWait().until(d ->
+       {
+            try {
+                WebElement element = d.findElement(locator);
+                new Actions(d).scrollToElement(element);
+                String textMsg = element.getText();
+                if (!textMsg.isEmpty()) {
+                    return textMsg;
+                } else {
+                    return null;
+                }
+            } catch (Exception e) {
+                return null;
+            }
+        }
+     );
+
+
     }
 
 
-
 }
+
