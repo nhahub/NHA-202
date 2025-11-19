a2b587c2a78ccf5b400e104c8e78375edb209da2
AyaTallah22 <am4564506@gmail.com>
2025-11-19 17:31:18 +0200

    Aya:add loginpage to product test

--- ALX3_SWD6_G1_NHA-202/src/test/java/POMpatternTests/ProductsTest.java
+++ ALX3_SWD6_G1_NHA-202/src/test/java/POMpatternTests/ProductsTest.java
@@ -1,7 +1,10 @@
 package POMpatternTests;
+import POMpatternPages.LoginPage;
 import POMpatternPages.ProductsPage;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,6 +16,7 @@ import org.testng.annotations.Test;
 public class ProductsTest {
 
     WebDriver driver;
+    LoginPage loginPage;
     ProductsPage productpage;
 
 
@@ -20,22 +24,30 @@ public class ProductsTest {
     public void setUp() {
         //
         // Initialize WebDriver and  productpage
+        ChromeOptions options= new ChromeOptions();
+        options.addArguments("--start-maximized --guest");
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
         driver = new ChromeDriver();
+        loginPage=new LoginPage(driver);
         productpage = new ProductsPage(driver);
-        driver.navigate().to("https://www.saucedemo.com/inventory.html");
+        driver.navigate().to("https://www.saucedemo.com/");
 
     }
     @Test
     public void testAddProductsToCart() {
-        // Step 1: Verify Products are Displayed
+        //
+        //step 1:login page
+        loginPage.setLogin("standard_user", "secret_sauce");
+
+        // Step 2: Verify Products are Displayed
         String title =  productpage.getPageTitle();
         Assert.assertTrue(title.contains("Products"), "Page title does not contain 'Products'");
 
-        // Step 2: Add Products to Cart
+        // Step 3: Add Products to Cart
         productpage.addFirstProductToCart();
         productpage.addSecondProductToCart();
 
-        // Step 3: Go to Cart and Verify Total Number of Items in Cart
+        // Step 4: Go to Cart and Verify Total Number of Items in Cart
         productpage.goToCart();
         Assert.assertEquals(productpage.getCartItems().size(), 2,"Cart does not contain 2 items");
 
